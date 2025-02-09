ea1d534322f2dce81969632cb422ea3c01b16c00
Shahen Algoo <shahenalgoo@gmail.com>
2025-02-09 11:08:59 +0400

    quick prototyping
    game posters

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,21 +2,59 @@
 
 import { Button } from "@heroui/button";
 import { Card } from "@heroui/react";
+import { Image } from "@heroui/image";
+import NextImage from "next/image";
+import Link from "next/link";
 
 export default function Home() {
 	return (
 		<div className="container">
-			{/* <div
-				className="absolute inset-0 h-full w-full bg-[#121214] bg-[radial-gradient(#1a1a1c_1px,transparent_1px)] [background-size:16px_16px]"
-			></div> */}
-			{/* <div
-				className="absolute inset-0 h-full w-full bg-[#121214] bg-[radial-gradient(#1a1a1c_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"
-			></div> */}
-			<div className="grid grid-cols-4 gap-4">
-				<Card className="min-h-40">Test</Card>
-				<Card className="min-h-40">Test</Card>
-				<Card className="min-h-40">Test</Card>
-				<Card className="min-h-40">Test</Card>
+			<div className="grid grid-cols-4 gap-6">
+
+				<Link href={"/game"}>
+					<Image
+						as={NextImage}
+						isBlurred
+						src={`/poster-blitzer.jpg`}
+						alt="Blitzer"
+						width={680}
+						height={200}
+						className="h-auto transition-all border-2 border-transparent hover:border-violet-500"
+					/>
+				</Link>
+				<Link href={"/game"}>
+					<Image
+						as={NextImage}
+						isBlurred
+						src={`/poster-ballsort.jpg`}
+						alt="Blitzer"
+						width={680}
+						height={200}
+						className="h-auto transition-all border-2 border-transparent hover:border-violet-500"
+					/>
+				</Link>
+				<Link href={"/game"}>
+					<Image
+						as={NextImage}
+						isBlurred
+						src={`/poster-spacefarer.jpg`}
+						alt="Blitzer"
+						width={680}
+						height={200}
+						className="h-auto transition-all border-2 border-transparent hover:border-violet-500"
+					/>
+				</Link>
+				<Link href={"/game"}>
+					<Image
+						as={NextImage}
+						isBlurred
+						src={`/poster-matchtwo.jpg`}
+						alt="Blitzer"
+						width={680}
+						height={200}
+						className="h-auto transition-all border-2 border-transparent hover:border-violet-500"
+					/>
+				</Link>
 			</div>
 		</div>
 	);
