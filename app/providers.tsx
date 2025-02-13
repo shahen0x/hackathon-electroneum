90971b9596f45af4f4536ebf7839f79a0324e932
Shahen Algoo <shahenalgoo@gmail.com>
2025-02-13 14:43:59 +0400

    added react query

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,8 +3,13 @@
 import { HeroUIProvider } from "@heroui/react";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ThirdwebProvider } from "thirdweb/react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+// Convex
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+// React Quary
+const queryClient = new QueryClient()
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
@@ -12,7 +17,9 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 			<HeroUIProvider>
 				<ThirdwebProvider>
 					<ConvexProvider client={convex}>
-						{children}
+						<QueryClientProvider client={queryClient}>
+							{children}
+						</QueryClientProvider>
 					</ConvexProvider>
 				</ThirdwebProvider>
 			</HeroUIProvider>
