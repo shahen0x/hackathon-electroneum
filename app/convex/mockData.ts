76ff89f13de30cad12cd1267d0a17b42e16abc35
Shahen Algoo <shahenalgoo@gmail.com>
2025-03-01 15:01:33 +0400

    updated mockdata on backend

--- app/convex/mockData.ts
+++ app/convex/mockData.ts
@@ -25,17 +25,45 @@ export const createMockPoolOwners = mutation({
 	handler: async (ctx) => {
 		const poolOwners = [
 			{
+				status: "active",
+				poolPrice: 2500,
 				tokenSymbol: "ETN",
 				tokenLogo: "https://s2.coinmarketcap.com/static/img/coins/64x64/2137.png",
 				tokenAddress: "0x0000000000000000000000000000000000000000",
 				payoutAddress: "0x37E5831239785039Ce8A76AfF44AD0E53AA25c8C"
 			},
 			{
+				status: "active",
+				poolPrice: 1000,
 				tokenSymbol: "MEME",
 				tokenLogo: "https://wary-raccoon-546.convex.cloud/api/storage/48404d7f-ec8b-4830-9293-cb3155cfc0ba",
 				tokenAddress: "0xdbE388e37794646461b1c3560838a1453001d1ef",
 				payoutAddress: "0x37E5831239785039Ce8A76AfF44AD0E53AA25c8C"
 			},
+			{
+				status: "upcoming",
+				poolPrice: 0,
+				tokenSymbol: "Buddy",
+				tokenLogo: "https://app.electroswap.io/images/0x38B54f147303887BD2E932373432FfCBD11Ff6a5.png",
+				tokenAddress: "0x38B54f147303887BD2E932373432FfCBD11Ff6a5",
+				payoutAddress: "0x00"
+			},
+			{
+				status: "upcoming",
+				poolPrice: 0,
+				tokenSymbol: "PDY",
+				tokenLogo: "https://app.electroswap.io/images/0xc20d02538368D8F7deBeAeB99D9a8b4d4D1DDC1C.png",
+				tokenAddress: "0xc20d02538368D8F7deBeAeB99D9a8b4d4D1DDC1C",
+				payoutAddress: "0x00"
+			},
+			{
+				status: "upcoming",
+				poolPrice: 0,
+				tokenSymbol: "MEGA",
+				tokenLogo: "https://pbs.twimg.com/profile_images/1889018389420777472/YIOt0clC_400x400.jpg",
+				tokenAddress: "0x00",
+				payoutAddress: "0x00"
+			}
 		] as Doc<"poolOwners">[];
 
 		for (const poolOwner of poolOwners) {
