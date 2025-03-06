97733a37bda5d93e00750594c5112b055c94d04b
Shahen Algoo <shahenalgoo@gmail.com>
2025-03-06 22:28:10 +0400

    moved cycle sorting and shuffling to the main fetch component

--- app/components/pools/pools.tsx
+++ app/components/pools/pools.tsx
@@ -15,35 +15,9 @@ const Pools = () => {
 	const { cycle, isLoading } = useCycleStore();
 
 
-	// Shuffle and sort function
-	//
-	function shuffleArray<T>(array: T[]): T[] {
-		return array
-			.map(item => ({ item, sort: Math.random() }))
-			.sort((a, b) => a.sort - b.sort)
-			.map(({ item }) => item);
-	}
-
-
-	// Sort active pools by ETN and shuffle the order of the rest
-	//
-	function sortActivePools(cycle: { activePools: any[] } | null | undefined) {
-		if (!cycle?.activePools || cycle.activePools.length === 0) return [];
-
-		const etnPool = cycle.activePools.find(pool => pool.tokenSymbol === "ETN");
-		const otherPools = cycle.activePools.filter(pool => pool.tokenSymbol !== "ETN");
-
-		return etnPool ? [etnPool, ...shuffleArray(otherPools)] : shuffleArray(otherPools);
-	}
-
-
-	// Sort the active pools
-	const sortedPools = sortActivePools(cycle);
-
-
 	return (
 		<div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-2">
-			{!isLoading && sortedPools.map((pool) => (
+			{!isLoading && cycle?.activePools.map((pool) => (
 				<PoolCardActive key={pool?.tokenSymbol} activePool={pool} />
 			))}
 
--- app/data/data.cycle.tsx
+++ app/data/data.cycle.tsx
@@ -1,9 +1,15 @@
+/**
+ * DATA CYCLE
+ * This component autofetches the current data cycle and updates the store
+ * It does not rerender because the data is the same during the cycle
+ * 
+ */
 import { useQuery } from "@tanstack/react-query";
 import { useConvex } from "convex/react";
 import { useEffect } from "react";
 import { api } from "~/convex/_generated/api";
 import { useCycleStore } from "~/store/store.cycle";
-
+import { Cycle } from "~/types/types.cycle";
 
 
 const DataCycle = () => {
@@ -14,23 +20,42 @@ const DataCycle = () => {
 	// Hooks
 	const convex = useConvex();
 
-	// Fetch data function
-	const fetchCycleWithPools = async () => {
-		console.log("🔃 Fetching cycle data...");
-		return await convex.query(api.pools.getActiveCycleWithPools);
-	}
 
 	// Fetch and set cycle store
+	//
 	const { data: cycleData, error: cycleError, isError: isCycleError } = useQuery({
 		queryKey: ["cycleData"],
-		queryFn: fetchCycleWithPools,
+		queryFn: async () => await convex.query(api.pools.getActiveCycleWithPools),
 		enabled: !cycle,
 		staleTime: 1000 * 60 * 30, // 30mins
 	});
 
+
+	// Function to sort activePools
+	//
+	function sortActivePools(cycle: Cycle | null): Cycle | null {
+		if (!cycle || !cycle.activePools || cycle.activePools.length === 0) return cycle;
+
+		const etnPool = cycle.activePools.find(pool => pool.tokenSymbol === "ETN");
+		const otherPools = cycle.activePools.filter(pool => pool.tokenSymbol !== "ETN");
+
+		return {
+			...cycle,
+			activePools: etnPool ? [etnPool, ...shuffleArray(otherPools)] : shuffleArray(otherPools),
+		};
+	}
+
+	function shuffleArray<T>(array: T[]): T[] {
+		return array
+			.map(item => ({ item, sort: Math.random() }))
+			.sort((a, b) => a.sort - b.sort)
+			.map(({ item }) => item);
+	}
+
 	useEffect(() => {
 		if (cycleData) {
-			setCycle(cycleData);
+			const sortedCycleData = sortActivePools(cycleData);
+			setCycle(sortedCycleData);
 			setIsLoading(false);
 		} else if (isCycleError) {
 			console.error(cycleError);
