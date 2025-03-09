25d39db70467028bf43ffcfab8b406e46d3ec458
Shahen Algoo <shahenalgoo@gmail.com>
2025-03-09 18:08:56 +0400

    Update chain.ts

--- app/config/chain.ts
+++ app/config/chain.ts
@@ -9,8 +9,8 @@ export const appMetaData = {
 
 export const chain = defineChain({
 	id: 52014,
-	// rpc: "https://rpc.ankr.com/electroneum",
-	rpc: "https://rpc.ankr.com/electroneum/52b5f737aef79782f352d60c4aff29a46ba25df38d7a71bfad0befd77cb17eb9",
+	rpc: "https://rpc.ankr.com/electroneum",
+	// rpc: "https://rpc.ankr.com/electroneum/52b5f737aef79782f352d60c4aff29a46ba25df38d7a71bfad0befd77cb17eb9",
 	nativeCurrency: {
 		name: "Electroneum",
 		symbol: "ETN",
